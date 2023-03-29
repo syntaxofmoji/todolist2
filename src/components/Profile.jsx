b5b6266e311634568d3da54dda50fd8cd11f2e84
syntaxofmoji <mojivariya@gmail.com>
2023-03-29 22:43:35 +0700

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src ="assets/moji.jpg" alt ="profile"/>
+            <h1 className="Name">Moji</h1>
+            <hr></hr>
+        </div>
+
+    )
+}
+
+export default Profile;
